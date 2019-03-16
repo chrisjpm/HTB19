fb6bfb4afed6bc36a7ac91f41e6c060180f15aad
Chris <chrispercevalmaxwell@gmail.com>
2019-03-16 16:51:42 +0000

    More stuff lmao

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,15 @@
+$(document).ready(function() {
+  $("button[name='classic-ins']").on("click", function() {
+    $("div.classic-ins").addClass('active');
+  });
+  $("a[href='#close']").on("click", function() {
+    $("div.classic-ins").removeClass('active');
+  });
 
+  $("button[name='insta-ins']").on("click", function() {
+    $("div.insta-ins").addClass('active');
+  });
+  $("a[href='#close']").on("click", function() {
+    $("div.insta-ins").removeClass('active');
+  });
+});
