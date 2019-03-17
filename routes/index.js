306fb2aaf37c5d9719b612e00cf2fa7c280ab975
Chris <chrispercevalmaxwell@gmail.com>
2019-03-17 10:48:29 +0000

    name

--- routes/index.js
+++ routes/index.js
@@ -37,7 +37,7 @@ gfycat.authenticate((err, data) => {
 });
 
 router.get('/', function(req, res, next) {
-  res.render('index', { domain: '1-800-MEME', layout: 'layout.hbs', memes: prodReadyMemes});
+  res.render('index', { domain: '1-800-MEME', layout: 'layout.hbs', memes: prodReadyMemes, username: name});
 });
 
 
