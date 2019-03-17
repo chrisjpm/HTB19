9d62f84c25360b775bd080eb78cc3744e32fa81b
Chris <chrispercevalmaxwell@gmail.com>
2019-03-17 05:07:35 +0000

    bottom text yeeyee

--- routes/test.js
+++ routes/test.js
@@ -6,6 +6,41 @@ var Jimp = require('jimp');
 
 router.get('/', function(req, res, next) {
 
+  Jimp.read('public/res/noise.jpg', (err, img) => {
+    if (err) throw err;
+    Jimp.loadFont(Jimp.FONT_SANS_32_WHITE).then(font => {
+      if (err) throw err;
+      img
+        .print(
+          font,
+          0,
+          0, {
+            text: 'Top text',
+            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+            alignmentY: Jimp.VERTICAL_ALIGN_TOP
+          },
+          img.bitmap.width,
+          (err, image, {
+            x,
+            y
+          }) => {
+            img.print(
+              font,
+              0,
+              img.bitmap.height - 40,
+              {
+                text: 'Bottom text',
+                alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+                alignmentY: Jimp.VERTICAL_ALIGN_BOTTOM
+              },
+              img.bitmap.width
+            );
+          }
+        )
+        .write('public/res/noise2.jpg');
+    });
+  });
+
   res.render('test', {
     domain: '1-800-MEME',
     layout: 'layout.hbs'
@@ -21,10 +56,15 @@ function deepFry(upload) {
         .contrast(0.6)
         .brightness(-0.1)
         .posterize(6)
-        .color([
-          { apply: 'saturate', params: [60] }
+        .color([{
+          apply: 'saturate',
+          params: [60]
+        }])
+        .convolute([
+          [-2, -1, 0],
+          [-1, 1, 1],
+          [0, 1, 2]
         ])
-        .convolute([[-2, -1, 0], [-1, 1, 1], [0, 1, 2]])
         .write('public/res/iloveit2.jpg'); // save
     });
   });
